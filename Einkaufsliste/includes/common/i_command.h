7ede5b108a604135eff2c7a8c13d8198132feb2d
Andreas Edmeier <andreas.edmeier@web.de>
2021-05-21 13:30:50 +0200

    added command interface

---
+++ Einkaufsliste/includes/common/i_command.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+namespace common {
+class ICommand
+{
+public:
+	virtual ~ICommand() = default;
+
+	static void Execute(std::unique_ptr<ICommand> aCommand);
+	static void ReExecute();
+	static void Revert();
+
+	virtual std::unique_ptr<ICommand> Clone() = 0;
+
+protected:
+	// returns wether or not this command is reversable
+	virtual bool DoExecute() = 0;
+	virtual void DoRevert() {};
+
+private:
+	static std::vector<std::unique_ptr<ICommand>> myCommandHistory;
+	static size_t myBackOffset;
+};
+} // namespace common
