96231d02b974193d34dd1eee98927f89fb9bd0a8
Andreas Edmeier <assertores@web.de>
2021-10-31 22:07:25 +0100

    rewrite cli interpretion part and add help

--- src/command_line_interface.cpp
+++ src/command_line_interface.cpp
@@ -4,6 +4,71 @@
 #include "interface/i_logger.h"
 
 namespace biz {
+CommandLineInterface::CommandLineInterface(
+	std::istream& aInput,
+	std::ostream& aOutput,
+	CliCommands aCommands)
+	: myInput(aInput)
+	, myOutput(aOutput)
+	, myCommands(std::move(aCommands))
+{
+	myInterpreter["undo"] = [this]() {
+		myCommandChain.Undo();
+	};
+	myInterpreter["redo"] = [this]() {
+		myCommandChain.Redo();
+	};
+	myInterpreter["open recipe"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myOpenRecipeCommand->Execute());
+	};
+	myInterpreter["change recipe name"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myChangeNameOfRecipeCommand->Execute());
+	};
+	myInterpreter["change recipe description"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myChangeDescriptionOfRecipeCommand->Execute());
+	};
+	myInterpreter["add recipe ingredient"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myAddIngredientToRecipeCommand->Execute());
+	};
+	myInterpreter["remove recipe ingredient"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myRemoveIngredientToRecipeCommand->Execute());
+	};
+	myInterpreter["print"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myPrintCurrentFileCommand->Execute());
+	};
+	myInterpreter["open convertion"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myOpenConvertionCommand->Execute());
+	};
+	myInterpreter["open week"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myOpenWeekCommand->Execute());
+	};
+	myInterpreter["start list"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myStartList->Execute());
+	};
+	myInterpreter["add week to list"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myAddWeekToListCommand->Execute());
+	};
+	myInterpreter["add recipe to list"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myAddRecipeToListCommand->Execute());
+	};
+	myInterpreter["compile"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myCompileListCommand->Execute());
+	};
+	myInterpreter["add recipe to week"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myAddRecipeToWeekCommand->Execute());
+	};
+	myInterpreter["remove recipe from week"] = [this]() {
+		myCommandChain.AddCommand(myCommands.myRemoveRecipeFromWeekCommand->Execute());
+	};
+	myInterpreter["help"] = [this]() {
+		for (const auto& it : myInterpreter)
+		{
+			myOutput << it.first << '\n';
+		}
+	};
+	// TODO: add funktionality here
+};
+
 std::filesystem::path
 CommandLineInterface::AskForFile()
 {
@@ -92,94 +157,18 @@ CommandLineInterface::Poll()
 	{
 		return true;
 	}
-	// TODO(andreas): help
-	if (command == "undo")
-	{
-		myCommandChain.Undo();
-		return false;
-	}
-	if (command == "redo")
-	{
-		myCommandChain.Redo();
-		return false;
-	}
-	if (command == "open recipe")
-	{
-		myCommandChain.AddCommand(myCommands.myOpenRecipeCommand->Execute());
-		return false;
-	}
-	if (command == "change recipe name")
-	{
-		myCommandChain.AddCommand(myCommands.myChangeNameOfRecipeCommand->Execute());
-		return false;
-	}
-	if (command == "change recipe description")
-	{
-		myCommandChain.AddCommand(myCommands.myChangeDescriptionOfRecipeCommand->Execute());
-		return false;
-	}
-	if (command == "add recipe ingredient")
-	{
-		myCommandChain.AddCommand(myCommands.myAddIngredientToRecipeCommand->Execute());
-		return false;
-	}
-	if (command == "remove recipe ingredient")
-	{
-		myCommandChain.AddCommand(myCommands.myRemoveIngredientToRecipeCommand->Execute());
-		return false;
-	}
-	if (command == "print")
-	{
-		myCommandChain.AddCommand(myCommands.myPrintCurrentFileCommand->Execute());
-		return false;
-	}
-	if (command == "open convertion")
-	{
-		myCommandChain.AddCommand(myCommands.myOpenConvertionCommand->Execute());
-		return false;
-	}
-	if (command == "open week")
-	{
-		myCommandChain.AddCommand(myCommands.myOpenWeekCommand->Execute());
-		return false;
-	}
-	if (command == "start list")
-	{
-		myCommandChain.AddCommand(myCommands.myStartList->Execute());
-		return false;
-	}
-	if (command == "add week to list")
-	{
-		myCommandChain.AddCommand(myCommands.myAddWeekToListCommand->Execute());
-		return false;
-	}
-	if (command == "add recipe to list")
-	{
-		myCommandChain.AddCommand(myCommands.myAddRecipeToListCommand->Execute());
-		return false;
-	}
-	if (command == "compile")
-	{
-		myCommandChain.AddCommand(myCommands.myCompileListCommand->Execute());
-		return false;
-	}
-	if (command == "add recipe to week")
-	{
-		myCommandChain.AddCommand(myCommands.myAddRecipeToWeekCommand->Execute());
-		return false;
-	}
-	if (command == "remove recipe from week")
-	{
-		myCommandChain.AddCommand(myCommands.myRemoveRecipeFromWeekCommand->Execute());
-		return false;
-	}
-	// TODO: add funktionality here
 
-	interface::ILogger::Log(
-		interface::LogLevel::Error,
-		interface::LogType::Commands,
-		"command '" + command + "' is unknowen.");
+	auto it = myInterpreter.find(command);
+	if (it == myInterpreter.end())
+	{
+		interface::ILogger::Log(
+			interface::LogLevel::Error,
+			interface::LogType::Commands,
+			"command '" + command + "' is unknowen.");
 
+		return false;
+	}
+	it->second();
 	return false;
 }
 } // namespace biz
