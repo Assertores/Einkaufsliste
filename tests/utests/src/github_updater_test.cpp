6e72ad4957ca1e2edd8bb504988ea22cc1de4580
Andreas Edmeier <assertores@web.de>
2021-11-29 22:39:35 +0100

    add test for unpacking

--- tests/utests/src/github_updater_test.cpp
+++ tests/utests/src/github_updater_test.cpp
@@ -55,11 +55,46 @@ public:
                 "assets": [ {
                     "url": "https://api.github.com/repos/octocat/Hello-World/releases/assets/1",
                     "browser_download_url": ")json"
-			+ myDownloadUrl + R"json(",
+			+ myDownloadUrlWin + R"json(",
                     "id": 1,
                     "node_id": "MDEyOlJlbGVhc2VBc3NldDE=",
                     "name": ")json"
-			+ myAssetName + R"json(",
+			+ myAssetNameWin + R"json(",
+                    "label": "short description",
+                    "state": "uploaded",
+                    "content_type": "application/zip",
+                    "size": 1024,
+                    "download_count": 42,
+                    "created_at": "2013-02-27T19:35:32Z",
+                    "updated_at": "2013-02-27T19:35:32Z",
+                    "uploader": {
+                        "login": "octocat",
+                        "id": 1,
+                        "node_id": "MDQ6VXNlcjE=",
+                        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                        "gravatar_id": "",
+                        "url": "https://api.github.com/users/octocat",
+                        "html_url": "https://github.com/octocat",
+                        "followers_url": "https://api.github.com/users/octocat/followers",
+                        "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                        "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                        "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                        "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                        "organizations_url": "https://api.github.com/users/octocat/orgs",
+                        "repos_url": "https://api.github.com/users/octocat/repos",
+                        "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                        "received_events_url": "https://api.github.com/users/octocat/received_events",
+                        "type": "User",
+                        "site_admin": false
+                    }
+                }, {
+                    "url": "https://api.github.com/repos/octocat/Hello-World/releases/assets/1",
+                    "browser_download_url": ")json"
+			+ myDownloadUrlLin + R"json(",
+                    "id": 1,
+                    "node_id": "MDEyOlJlbGVhc2VBc3NldDE=",
+                    "name": ")json"
+			+ myAssetNameLin + R"json(",
                     "label": "short description",
                     "state": "uploaded",
                     "content_type": "application/zip",
@@ -97,8 +132,10 @@ public:
 	std::filesystem::path GetExePath() override { return locAssetDir; }
 
 	std::string myTestVersion = "v1.0.0";
-	std::string myAssetName = "Einkaufsliste_1.0.0_win10.zip";
-	std::string myDownloadUrl = "www.something.com/to/download";
+	std::string myAssetNameWin = "Einkaufsliste_1.0.0_win10.zip";
+	std::string myAssetNameLin = "Einkaufsliste_1.0.0_linux.zip";
+	std::string myDownloadUrlWin = "www.something.com/to/download/win10.zip";
+	std::string myDownloadUrlLin = "www.something.com/to/download/linux.zip";
 };
 
 class GithubTestFixture : public ::testing::Test {
@@ -109,13 +146,31 @@ protected:
 			locAssetDir,
 			std::filesystem::copy_options::overwrite_existing
 				| std::filesystem::copy_options::recursive);
-		common::LogOnConsole logger(std::cout);
+
 		infas::ILogger::SetLogLevel(infas::LogLevel::Verbose);
 		infas::ILogger::SetLogMask(infas::locLogMaskAll);
-		infas::ILogger::SetImplimentation(std::move(logger));
 	}
 
-	void TearDown() override { std::filesystem::remove_all(locAssetDir); }
+	void TearDown() override {
+		if (HasFailure()) {
+			infas::ILogger::SetImplimentation(common::LogOnConsole(std::cout));
+			std::cout << "===== ===== assetContent ===== =====\n";
+			for (const auto& it : std::filesystem::recursive_directory_iterator(
+					 std::filesystem::path(locAssetDir))) {
+				std::cout << " > " << it.path().u8string() << '\n';
+				if (it.is_regular_file()) {
+					auto file = std::ifstream(it);
+					std::cout << std::string(
+						(std::istreambuf_iterator<char>(file)),
+						(std::istreambuf_iterator<char>()))
+							  << '\n';
+					file.close();
+				}
+			}
+		}
+
+		std::filesystem::remove_all(locAssetDir);
+	}
 };
 
 TEST_F(GithubTestFixture, higher_version_is_identifyed_as_patch)  // NOLINT
@@ -159,5 +214,37 @@ TEST_F(GithubTestFixture, download_url_is_read_out_from_json)  // NOLINT
 	GithubUpdaterStub subject;
 	subject.RetraveMetaData();
 	ASSERT_TRUE(subject.RetreavePatchLocation());
-	EXPECT_EQ(subject.myPatch, subject.myDownloadUrl);
+#if _WIN32
+	EXPECT_EQ(subject.myPatch, subject.myDownloadUrlWin);
+#elif __linux__
+	EXPECT_EQ(subject.myPatch, subject.myDownloadUrlLin);
+#else
+#error "unsupporded platform"
+#endif
+}
+
+TEST_F(GithubTestFixture, patch_zip_is_unpacked)  // NOLINT
+{
+	GithubUpdaterStub subject;
+	subject.RetraveMetaData();
+	ASSERT_TRUE(subject.ExtractPatch());
+
+	EXPECT_TRUE(std::filesystem::exists(subject.GetPatchPath()));
+	EXPECT_TRUE(std::filesystem::exists(subject.GetPatchPath() / "stuff.txt"));
+	EXPECT_TRUE(std::filesystem::exists(subject.GetPatchPath() / "subfolder"));
+	EXPECT_TRUE(std::filesystem::exists(subject.GetPatchPath() / "subfolder" / "moreStuff.txt"));
+
+	auto stuffFile = std::ifstream(subject.GetPatchPath() / "stuff.txt");
+	const std::string stuffContent(
+		(std::istreambuf_iterator<char>(stuffFile)),
+		(std::istreambuf_iterator<char>()));
+	stuffFile.close();
+	EXPECT_EQ(stuffContent, "content");
+
+	auto moreStuffFile = std::ifstream(subject.GetPatchPath() / "subfolder" / "moreStuff.txt");
+	const std::string moreStuffContent(
+		(std::istreambuf_iterator<char>(moreStuffFile)),
+		(std::istreambuf_iterator<char>()));
+	moreStuffFile.close();
+	EXPECT_EQ(moreStuffContent, "different content");
 }
