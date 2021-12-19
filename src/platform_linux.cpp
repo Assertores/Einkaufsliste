1640542ec09f47bc03e0983f7d0eb503511e2150
Andreas Edmeier <assertores@web.de>
2021-12-19 15:40:50 +0100

    add linux start process implimentation

--- src/platform_linux.cpp
+++ src/platform_linux.cpp
@@ -1,5 +1,7 @@
 #include "common/platform.h"
 
+#include <spawn.h>
+
 namespace common {
 std::string
 PlatformIdentifyer() {
@@ -11,7 +13,15 @@ StartProcess(
 	const std::filesystem::path& aExe,
 	const std::vector<std::string_view>& aCommands,
 	const std::filesystem::path& aWorkingDirectory) {
-	static_assert(false, "not implimented");
+	std::vector<std::string> argCopy(aCommands.cbegin(), aCommands.cend());
+	std::vector<char*> args{};
+	for (auto& it : argCopy) {
+		args.push_back(it.data());
+	}
+	args.push_back(nullptr);
+
+	pid_t pid{};
+	posix_spawn(&pid, aExe.native().c_str(), nullptr, nullptr, args.data(), nullptr);
 }
 
 std::filesystem::path
